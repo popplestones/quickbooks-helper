2ad98de075ea74b466f347544fa70c2ee6be39f0
Shane Poppleton <shane@bjja.com.au>
2022-08-02 11:44:57 +1000

    move adjustment synced_at update outside line loop

--- src/Console/Commands/QbAdjustmentImport.php
+++ src/Console/Commands/QbAdjustmentImport.php
@@ -70,9 +70,9 @@ class QbAdjustmentImport extends Command
                         }
                         $this->adjustmentLineModel::create($this->setLineMapping($line, $this->lineMapping,
                             $adjustment));
-                        
-                        $adjustment->update([$this->mapping['synced_at'] => now()]);
+
                     });
+                    $adjustment->update([$this->mapping['synced_at'] => now()]);
                 },
                 activeFilter: false
             );
