8f94df22a988564f517eac79ad24fc24b6ac58b6
Shane Poppleton <shane@bjja.com.au>
2022-08-02 14:47:01 +1000

    create invoice sync

--- src/Console/Commands/QbInvoiceSync.php
+++ src/Console/Commands/QbInvoiceSync.php
@@ -3,9 +3,34 @@
 namespace Popplestones\Quickbooks\Console\Commands;
 
 use Illuminate\Console\Command;
+use Popplestones\Quickbooks\Facades\CallbackManager;
+use Popplestones\Quickbooks\Services\QuickbooksHelper;
+use QuickBooksOnline\API\Facades\Invoice;
+use QuickBooksOnline\API\Facades\Payment;
 
 class QbInvoiceSync extends Command
 {
+    use SyncsWithQuickbooks;
+
+    const MAX_FAILED = 3;
+
+    public $model;
+    public $mapping;
+    public $callbacks;
+    public $qb_helper;
+    public $objMethod;
+    public $apiMethod;
+    public $max_failed;
+
+    private function setup()
+    {
+        $this->mapping   = config('quickbooks.invoice.attributeMap');
+        $this->qb_helper = new QuickbooksHelper();
+        $this->callbacks = CallbackManager::getCallbacks('invoices');
+        $this->max_failed = self::MAX_FAILED;
+        $this->objMethod = '';
+        $this->apiMethod = '';
+    }
     /**
      * The name and signature of the console command.
      *
@@ -27,6 +52,113 @@ class QbInvoiceSync extends Command
      */
     public function handle()
     {
+        $this->setup();
+        if (!$this->checkConnection()) return 1;
+
+        $this->newLine();
+        $this->components->info("Syncing invoices to quickbooks.");
+
+        $query = ($this->callbacks->query)();
+        $this->applyIdOption($query);
+        $query->limit($this->option('limit'));
+
+        $invoices = $query->get();
+
+        if ($invoices->count() === 0)
+            $this->info('No invoices to sync');
+
+        $this->output->progressStart($invoices->count());
+
+        $invoices->each(function ($invoice) {
+            try {
+                $invoice_params[] = $this->prepareData($invoice);
+                $this->objMethod = 'create';
+                $this->apiMethod = 'Add';
+
+                $error = $this->getExistingRecord('Invoice', 'qb_invoice_id', $invoice, $invoice_params);
+                if ($error) return true;
+
+                $this->newLine();
+                $this->info("Invoice Params used to create local QbInvoice Object:");
+                $this->info(json_encode($invoice_params));
+
+                $qbInvoice = Invoice::{$this->objMethod}(...$invoice_params);
+
+                $this->info("Local QbInvoice object:");
+                $this->info(json_encode($qbInvoice));
+
+
+                $this->info("Calling {$this->apiMethod}");
+                $result = $this->qb_helper->dsCall($this->apiMethod, $qbInvoice);
+
+                if ($result) {
+                    $invoice->{$this->mapping['qb_invoice_id']} = $result->Id;
+                }
+                else {
+                    $this->warn('Adding invoice failed!');
+                    $error = $this->qb_helper->dataService->getLastError();
+                    $this->warn($error->getResponseBody());
+                }
+
+                $this->info("Updating synced_at");
+                $invoice->fill([
+                    'timestamps' => false,
+                    'synced_at' => now()
+                ]);
+
+                $invoice->save();
+            } catch (\Exception $e) {
+                $this->info("Some exception occurred");
+                $this->syncFailed($e, $invoice, 'invoice');
+                return false;
+            }
+            $this->output->progressAdvance();
+        });
+        $this->output->progressFinish();
+
         return 0;
+    }
+
+    private function prepareLineData($invoice)
+    {
+        $invoice->invoiceLines->map(fn($line) => (object)[
+            'Description' => $line->description,
+            'DetailType' => $line->detail_type,
+            'ItemRef' => (object)[
+                'name' => $line->item->name,
+                'value' => $line->item->qb_item_id],
+            'Amount' => $line->amount,
+            'LineNum' => $line->line_num,
+            'Qty' => $line->qty,
+            'UnitPrice' => $line->unit_price
+        ])->toArray();
+    }
+    private function prepareData($invoice)
+    {
+        return array_filter([
+            'TxnDate' => data_get($invoice, $this->mapping['transaction_date']),
+            'TotalAmt' => data_get($invoice, $this->mapping['total_amount']),
+            'Line' => $this->prepareLineData($invoice),
+            'DueDate' => data_get($invoice, $this->mapping['due_date']),
+            'CustomerMemo' => (object)[
+                'value' => data_get($invoice, $this->mapping['customer_memo']),
+            ],
+            'Balance' => data_get($invoice, $this->mapping['balance']),
+            'CustomerRef' => (object)[
+                'name' => $invoice->customer->company_name,
+                'value' => $invoice->customer->qb_customer_id
+            ],
+            'BillEmail' => (object)[
+                'Address' => $invoice->bill_email
+            ],
+            'BillAddr' => (object)[
+                'Line4' => '',
+                'Line3' => '',
+                'Line2' => '',
+                'Line1' => ''
+            ],
+        ], function ($val) {
+            return ! is_null($val);
+        });
     }
 }
